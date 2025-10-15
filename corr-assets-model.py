bc5a5a3edfe0a9a8106bb05ff6a6a8a2251f32cb
essentiapip <112524957+essentiapip@users.noreply.github.com>
2025-10-15 01:05:57 -0400

    Add files via upload

--- corr-assets-model.py
+++ corr-assets-model.py
@@ -53,6 +53,8 @@ connection_session.mount('https://', adapter)
 full_basket_list = []
 times = []
 
+trade_list = []
+
 # date = trading_dates[0]
 for date in trading_dates:
     
@@ -120,7 +122,7 @@ for date in trading_dates:
         for sample_ticker in sample_tickers:
             
             sample_ticker_data = full_ticker_prices[full_ticker_prices["ticker"] == sample_ticker].copy().sort_index()
-            ticker_date_universe = full_ticker_prices[(full_ticker_prices["date"] == ticker_date) & (full_ticker_prices["ticker"] != sample_ticker)].copy()#.sort_index()
+            ticker_date_universe = full_ticker_prices[(full_ticker_prices["date"] == prior_date) & (full_ticker_prices["ticker"] != sample_ticker)].copy()#.sort_index()
 
             tickers_on_date = ticker_date_universe["ticker"].drop_duplicates().values
             
@@ -131,12 +133,12 @@ for date in trading_dates:
             
                 sample_ticker_data_for_comparison = full_ticker_prices[full_ticker_prices["ticker"] == date_ticker].copy().sort_index()
                 
-                ticker_corr_sample = pd.merge(sample_ticker_data[["date","c", "pct_chg"]], sample_ticker_data_for_comparison[["date","c", "pct_chg"]], on = "date")
+                ticker_corr_sample = pd.merge(sample_ticker_data, sample_ticker_data_for_comparison, on = "date")
                 
                 px_corr = ticker_corr_sample["c_x"].corr(ticker_corr_sample["c_y"])
                 ret_corr = ticker_corr_sample["pct_chg_x"].corr(ticker_corr_sample["pct_chg_y"])
                 
-                ticker_corr_data = pd.DataFrame([{"ticker": date_ticker, "px_corr": px_corr, "ret_corr": ret_corr}])
+                ticker_corr_data = pd.DataFrame([{"ticker": date_ticker, "px_corr": px_corr, "ret_corr": ret_corr, "long_wt": (ticker_corr_sample["vol_y"].iloc[-1] / ticker_corr_sample["vol_x"].iloc[-1])}])
                 
                 ticker_corr_list.append(ticker_corr_data)
                 
@@ -145,46 +147,32 @@ for date in trading_dates:
             most_correlated = full_ticker_corr_data.head(1)
             
             corr_pair_data = pd.DataFrame([{"date": date, "long_ticker": sample_ticker, "short_ticker": most_correlated["ticker"].iloc[0],
-                                            "px_corr": most_correlated["px_corr"].iloc[0], "ret_corr": most_correlated["ret_corr"].iloc[0]}])
+                                            "px_corr": most_correlated["px_corr"].iloc[0], "ret_corr": most_correlated["ret_corr"].iloc[0],
+                                            "long_wt": most_correlated["long_wt"].iloc[0]}])
             
             corr_pair_list.append(corr_pair_data)
             
-        full_corr_pairs = pd.concat(corr_pair_list)
-            
-            
-    
-tickers = np.array([["RGTI", "IONQ"], ["RIOT", "MARA"], ["OKLO","SMR"], ["CVNA", "KMX"]])
-
-target_pnl = 1
-
-# Keep 1 session active (same headers, etc.)
-connection_session = requests.Session()
-adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=500)
-connection_session.mount('https://', adapter)
-
-notional = 10000
-
-all_trades_list = []
-
-# ticker_set = tickers[0]
-for ticker_set in tickers:
-    try:
+        full_corr_pairs = pd.concat(corr_pair_list).reset_index(drop=True)
         
-        long_ticker = ticker_set[0]
-        short_ticker = ticker_set[1]
+        target_pnl = 1
+
+        notional = 10000
         
-        # =============================================================================
-        # Modeling
-        # =============================================================================
+        lookback_date_range = np.sort(all_trading_dates[all_trading_dates < date])[-20:]
         
-        intraday_data_list = []
-        
-        # date = trading_dates[0]
-        for date in trading_dates:
+        # pair = full_corr_pairs.index[0]
+        for pair in full_corr_pairs.index:
             
             try:
             
-                date_info = corr_data[corr_data["date"] == date].copy()
+                pair_data = full_corr_pairs[full_corr_pairs.index == pair].copy()
+                
+                long_ticker = pair_data["long_ticker"].iloc[0]
+                short_ticker = pair_data["short_ticker"].iloc[0]
+            
+                # =============================================================================
+                # Modeling
+                # =============================================================================
                 
                 long_ticker_data = pd.json_normalize(connection_session.get(f"https://api.polygon.io/v2/aggs/ticker/{long_ticker}/range/1/minute/{date}/{date}?adjusted=true&sort=asc&limit=50000&apiKey={polygon_api_key}").json()["results"]).set_index("t")
                 long_ticker_data.index = pd.to_datetime(long_ticker_data.index, unit="ms", utc=True).tz_convert("America/New_York")    
@@ -202,69 +190,53 @@ for ticker_set in tickers:
             
                 combined_intraday_data = pd.merge(long_ticker_data[["date", "intraday_returns"]], short_ticker_data[["intraday_returns"]], left_index=True, right_index=True)    
                 
-                combined_intraday_data["long_wt"] = date_info["long_wt"].iloc[0]
+                combined_intraday_data["long_wt"] = pair_data["long_wt"].iloc[0]
                 
                 combined_intraday_data["adj_long_returns"] = combined_intraday_data["intraday_returns_x"] * combined_intraday_data["long_wt"]
                 
                 combined_intraday_data["gross_return"] = combined_intraday_data["adj_long_returns"] - combined_intraday_data["intraday_returns_y"]
                 
-                intraday_data_list.append(combined_intraday_data)
+                signals = combined_intraday_data[combined_intraday_data["gross_return"] >= target_pnl].sort_index().copy()
+                
+                if len(signals) < 1:
+                    
+                    trade = pd.DataFrame([{"date": date, "long_ticker": long_ticker,
+                                           "short_ticker": short_ticker,
+                                           "return": combined_intraday_data["gross_return"].iloc[-1],
+                                           "signal": 0}])
+                    
+                else:
+                    
+                    trade = pd.DataFrame([{"date": date, "long_ticker": long_ticker,
+                                           "short_ticker": short_ticker, "return": signals["gross_return"].iloc[0],
+                                           "signal": 1}])
+                    
+                trade = pd.merge(pair_data, trade, on="date")
+                    
+                trade_list.append(trade)
+            
                 
             except Exception as error:
                 print(error)
                 continue
-        
-        full_dataset = pd.concat(intraday_data_list)
-        
-        # =============================================================================
-        # Trade Simulation
-        # =============================================================================
-        
-        dates_in_coverage = np.sort(full_dataset["date"].drop_duplicates().values)
-        
-        trade_list = []
-        
-        # trade_date = dates_in_coverage[0]
-        for trade_date in dates_in_coverage:
             
-            daily_trade_info = full_dataset[full_dataset["date"] == trade_date].copy().sort_index()
-            
-            signals = daily_trade_info[daily_trade_info["gross_return"] >= target_pnl].sort_index().copy()
-            
-            if len(signals) < 1:
-                
-                trade = pd.DataFrame([{"date": trade_date, "return": daily_trade_info["gross_return"].iloc[-1],
-                                       "signal": 0}])
-                
-                trade = pd.merge(daily_trade_info.tail(1), trade, on="date")
-            else:
-                
-                trade = pd.DataFrame([{"date": trade_date, "return": signals["gross_return"].iloc[0],
-                                       "signal": 1}])
-                
-                trade = pd.merge(daily_trade_info.tail(1), trade, on="date")
-                
-            trade_list.append(trade)
-            
-        all_trades = pd.concat(trade_list)
-        all_trades["long_ticker"] = long_ticker
-        all_trades["short_ticker"] = short_ticker
-        
-        all_trades["dollar_pnl"] = ((all_trades["gross_return"]/100) * notional)
-        
-        all_trades["return_on_size"] = round((all_trades["dollar_pnl"] / notional)*100, 2)
-        all_trades["rolling_std"] = all_trades["return_on_size"].rolling(window=5).std()
-        all_trades["capital"] = notional + all_trades["dollar_pnl"].cumsum()
-        
-        all_trades["strategy_return"] = round(((all_trades["capital"] - notional) / notional)*100, 2)
-        all_trades["strategy_sharpe"] = round(all_trades["strategy_return"] / (all_trades["rolling_std"] * np.sqrt(5)), 2)
-        
-        all_trades_list.append(all_trades)
-        
     except Exception as error:
         print(error)
         continue
-    
+            
+all_trades = pd.concat(trade_list)
+all_trades["long_ticker"] = long_ticker
+all_trades["short_ticker"] = short_ticker
+
+all_trades["dollar_pnl"] = ((all_trades["gross_return"]/100) * notional)
+
+all_trades["return_on_size"] = round((all_trades["dollar_pnl"] / notional)*100, 2)
+all_trades["rolling_std"] = all_trades["return_on_size"].rolling(window=5).std()
+all_trades["capital"] = notional + all_trades["dollar_pnl"].cumsum()
+
+all_trades["strategy_return"] = round(((all_trades["capital"] - notional) / notional)*100, 2)
+all_trades["strategy_sharpe"] = round(all_trades["strategy_return"] / (all_trades["rolling_std"] * np.sqrt(5)), 2)
+
     
 complete_trade_data = pd.concat(all_trades_list)
 
